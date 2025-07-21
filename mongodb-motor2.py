f672a046d74e8d2f3aa5f7db7e3dc6acf9a57a30
josevitor123271 <caang.2024119tads0019@aluno.ifpi.edu.br>
2025-07-21 17:10:32 -0300

    Com motor

---
+++ mongodb-motor2.py
@@ -0,0 +1,26 @@
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.server_api import ServerApi
+from pprint import pprint
+
+url = "mongodb+srv://caang2024119tads0019:n04k7hGgZ9NLbozk@zeneto.0rjs15h.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=ZeNeto"
+
+async def busque_filmes():
+    client = AsyncIOMotorClient(url, server_api=ServerApi('1'))
+    
+    try:
+        database = client['sample_mflix']
+        collection_movies = database['movies']
+        
+        query = {"title": "A Corner in Wheat"}
+
+        film = await collection_movies.find_one(query)
+        if film:
+            pprint("Found film: ", film)
+        else:
+            pprint("No film found!")
+            
+    except Exception as error:
+        print(error)
+    finally:
+        client.close()
